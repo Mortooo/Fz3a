e1f145ccc8a012bb4a9624a4c087f4ee012bb148
Mortooo <mortada33360@gmail.com>
2026-08-28 13:40:25 +0200

    Djano

---
+++ config/urls.py
@@ -0,0 +1,20 @@
+"""URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from home import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+]
+
+# Serve gallery files and the logo from the project root during development.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
